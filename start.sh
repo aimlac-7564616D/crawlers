c7a1eb911e9bcfc051a50abe6630473b2a57be5c
Fergus Baker <fergusbkr@gmail.com>
2022-02-15 02:02:22 +0000

    Open Weather API (#3)
    * updated flows for open weather map
    
    * updated start script with new env variables

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ docker run --rm  -p 1880:1880 \
     --env AIMLAC_RSE_ADDR=$AIMLAC_RSE_ADDR \
     --env MET_SECRET_KEY=$MET_SECRET_KEY \
     --env MET_API_KEY=$MET_API_KEY \
+    --env LOCATION_LAT=52.1051 \
+    --env LOCATION_LON=-3.6680 \
+    --env OPEN_WEATHER_API=$OPEN_WEATHER_API \
     -v $(pwd)/flows:/data \
     --name api-scraper \
     nodered/node-red
